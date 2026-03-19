4a0ee14a438f6fb1a0afac219eb9aa0395e337cf
LUCAS D'AVILA MARTINS <lucas.martins286@etec.sp.gov.br>
2026-03-19 11:52:26 -0300

    update 1.0000001

---
+++ src/servers/components/CardPersonal.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+import styles from "../styles/styles";
+import { deletePerson } from "../servers/peopleCrud";
+
+export default function CardPersonal({ item, navigation, refresh }) {
+  return(
+    <View style={styles.card}>
+
+      <View>
+        <Text style={styles.name}>
+          {item.firstName} {item.lastName}
+        </Text>
+
+        <Text style={styles.email}>
+          {item.email}
+        </Text>
+      </View>
+
+      <View>
+        <Button
+          title="Editar"
+          onPress={() => navigation.navigate("AddEdit", { person: item })}
+        />
+
+        <Button
+          title="Deletar"
+          onPress={async () => {
+            await deletePerson(item.id);
+            refresh();
+          }}
+        />
+      </View>
+
+    </View>
+  );
+}
